37980dcd8968e26235da722a8a54dbdf66f54698
joao.murari <joao_pichula@hotmail.com>
2025-04-04 17:21:52 -0300

    feat: main page is already done, but need so many changes

--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:weather_app/models/weather_models.dart';
 import 'package:weather_app/services/weather_services.dart';
 
@@ -25,6 +26,30 @@ class _WeatherPageState extends State<WeatherPage> {
     }
   }
 
+  String getWeatherAnimation(String? mainCondition){
+    if (mainCondition == null) return 'assets/sunny.json';
+
+    switch(mainCondition.toLowerCase()){
+      case 'clouds':
+      case 'mist':
+      case 'smoke':
+      case 'haze':
+      case 'dust':
+      case 'fog':
+        return 'assets/cloud.json';
+      case 'rain':
+      case 'drizzle':
+      case 'shower rain':
+        return 'assets/rain.json';
+      case 'thunderstorm':
+        return 'assets/thunder.json';
+      case 'clear':
+        return 'assets/sunny.json';
+      default:
+        return 'assets/sunny.json';
+    }
+  }
+
   @override
   void initState() {
     super.initState();
@@ -34,15 +59,18 @@ class _WeatherPageState extends State<WeatherPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[800],
       body: Center(
         child: _weather == null
             ? const CircularProgressIndicator() 
             : Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text(_weather!.cityName, style: const TextStyle(fontSize: 24)),
-                  const SizedBox(height: 8),
-                  Text('${_weather!.temperature.round()}ºC', style: const TextStyle(fontSize: 32)),
+                  Text(_weather?.cityName?? "Loading City"), // AQUI SERA O NOME DA CIDADE
+
+                  Lottie.asset(getWeatherAnimation(_weather?.mainCondition)),
+
+                  Text('${_weather!.temperature.round()}ºC'),
                 ],
               ),
       ),
